7214ffcb1a2a20b0af036e2e53c31e6f0b16d88c
muraleekm <muraleekrishnan.m@gmail.com>
2016-10-12 01:23:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,15 +17,15 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/article1', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.send('Article1 required and will be served');
 });
 
-app.get('/article2', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send('Article2 required and will be served');
 });
 
-app.get('/article3', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.send('Article3 required and will be served');
 });
 
