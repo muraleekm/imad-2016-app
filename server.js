df0b564125a2a216ad145a155a790bed11dd193e
muraleekm <muraleekrishnan.m@gmail.com>
2016-10-12 10:36:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content = {
+var articleOone = {
     title:'Article1-Muralee',
     heading:'Article One',
     date:'Sep 5 2016',
@@ -81,7 +81,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
